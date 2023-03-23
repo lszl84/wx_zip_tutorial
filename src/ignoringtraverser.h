c71e503a8f5846ab68b04f4f9ff524642eb86d6d
Luke <luke@justdevtutorials.com>
2023-03-23 15:50:16 +0100

    code cleanup

--- src/ignoringtraverser.h
+++ src/ignoringtraverser.h
@@ -5,21 +5,17 @@
 
 struct IgnoringTraverser : public wxDirTraverser
 {
-    // std::vector<std::string> files;
     std::function<bool(const std::string &)> shouldIgnoreDir;
     std::function<void(const std::string &)> fileEnterCallback;
 
     virtual wxDirTraverseResult OnFile(const wxString &filename) override
     {
-        // files.push_back(filename.ToStdString());
-
         fileEnterCallback(filename.ToStdString());
         return wxDIR_CONTINUE;
     }
 
     virtual wxDirTraverseResult OnDir(const wxString &dirname) override
     {
-        // This is ok. Ignoring any dir, also if inside
         auto lastPathComponent = wxFileName(dirname).GetFullName();
 
         return shouldIgnoreDir(lastPathComponent.ToStdString()) ? wxDIR_IGNORE : wxDIR_CONTINUE;
